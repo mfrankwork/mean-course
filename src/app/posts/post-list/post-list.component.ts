c8de2ec5b3e14c776929ac0a73f9edc0b9797e91
Michael Frank <mfrankwork@gmail.com>
2021-12-29 10:31:29 -0500

    Add form

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,15 +1,17 @@
 import { Component, Input } from '@angular/core';
 
+import { Post } from '../post.model';
+
 @Component({
   selector: 'app-post-list',
   templateUrl: './post-list.component.html',
   styleUrls: ['./post-list.component.css'],
 })
 export class PostListComponent {
-  // posts = [
+  // posts: Post[] = [
   //   { title: 'First Post', content: "This is the first post's content" },
   //   { title: 'Second Post', content: "This is the second post's content" },
   //   { title: 'Third Post', content: "This is the third post's content" },
   // ];
-  @Input() posts = [];
+  @Input() posts: Post[] = [];
 }
