1723abdc32872e592b091b54463b60ecca80307e
Michael Frank <mfrankwork@gmail.com>
2021-12-30 10:49:52 -0500

    Continue Express app

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,10 @@
+const bodyParser = require('body-parser');
 const express = require('express');
 
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
@@ -16,10 +20,14 @@ app.use((req, res, next) => {
 });
 
 app.post('/api/posts', (req, res, next) => {
-  console.log();
+  const post = req.body;
+  console.log(post);
+  res.status(201).json({
+    message: 'Post added successfully',
+  });
 });
 
-app.use('/api/posts', (req, res, next) => {
+app.get('/api/posts', (req, res, next) => {
   const posts = [
     {
       id: 'gdhsjg',
