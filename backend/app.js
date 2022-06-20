6c8aa0ed775f22f7d695f10fd2335850f3fabdf8
Michael Frank <mfrankwork@gmail.com>
2022-06-20 14:02:11 -0400

    Prettier backend

--- backend/app.js
+++ backend/app.js
@@ -16,23 +16,23 @@ const mongoHost = process.env.MONGODB_HOST;
 const mongoDatabase = process.env.MONGODB_DATABASE;
 const mongoConnStr = `mongodb+srv://${mongoUser}:${mongoPassword}@${mongoHost}/${mongoDatabase}?retryWrites=true&w=majority`;
 mongoose
-	.connect(mongoConnStr)
-	.then(() => {
-		console.log('Connected to database');
-	})
-	.catch(() => {
-		console.log('Connection failed!');
-	});
+  .connect(mongoConnStr)
+  .then(() => {
+    console.log('Connected to database');
+  })
+  .catch((err) => {
+    console.error('Connection failed!', err);
+  });
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false })); // NOTE: Not necessary for this application
 app.use('/images', express.static(path.join('backend/images')));
 
 app.use((req, res, next) => {
-	res.setHeader('Access-Control-Allow-Origin', '*');
-	res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE, OPTIONS');
-	next();
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE, OPTIONS');
+  next();
 });
 
 app.use('/api/posts', postRoutes);
